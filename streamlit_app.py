48e8fb536ffd815eab65006da76493cea757db2e
Cody Large <97487434+codylarge@users.noreply.github.com>
2024-11-17 23:17:17 +0000

    Cat and dog prediction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,37 @@
 import streamlit as st
+from inference_sdk import InferenceHTTPClient
+from PIL import Image
+from dotenv import load_dotenv
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# Get API key from .env
+load_dotenv()
+KEY = os.getenv("ROBOFLOW_API_KEY")
+
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key= KEY
 )
+
+MODEL_ID = "dog-and-cats/1" 
+
+st.title("RoboFlow Model Demo")
+st.write("Upload an image to run inference with the RoboFlow model.")
+
+# Image uploader
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file) # Upload
+    st.image(image, caption="Uploaded Image", use_container_width=True) # Display
+
+    # Save the image temporarily
+    temp_path = "temp_image.jpg"
+    image.save(temp_path)
+
+    st.write("Running inference...")
+    result = CLIENT.infer(temp_path, model_id=MODEL_ID)
+
+    # Display prediction results
+    st.write("Inference Results:")
+    st.json(result)
